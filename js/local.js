4db3b48e2256475dc5e92753f1a2989922547c8d
JonathanCho <jonrcao@gmail.com>
2017-08-27 21:40:31 +0800

    init

---
+++ js/local.js
@@ -0,0 +1,32 @@
+var Local = function () {
+  // 游戏对象
+  var game;
+  // 绑定键盘事件
+  var bindKeyEvent = function () {
+    document.onkeydown = function (e) {
+      if (e.keyCode == 38) { // up
+        game.rotate();
+      } else if (e.keyCode == 39) { // right
+        game.right();
+      } else if (e.keyCode == 40) { // down
+        game.down();
+      } else if (e.keyCode == 37) { // left
+        game.left();
+      } else if (e.keyCode == 32) { // space
+        game.fall();
+      }
+    }
+  }
+  // 开始
+  var start = function () {
+    var doms = {
+      gameDiv: document.getElementById('game'),
+      nextDiv: document.getElementById('next')
+    }
+    game = new Game();
+    game.init(doms);
+    bindKeyEvent();
+  }
+
+  this.start = start;
+}
